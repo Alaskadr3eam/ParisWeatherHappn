fd5384b27ccbba5569d8ca22a56ddd5eafee15b9
Clement <clement.martin1988@gmail.com>
2020-03-18 16:36:02 +0100

    init

---
+++ ParisHappnWeather/ParisHappnWeatherTests/ParisHappnWeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  ParisHappnWeatherTests.swift
+//  ParisHappnWeatherTests
+//
+//  Created by Clément Martin on 18/03/2020.
+//  Copyright © 2020 Clément Martin. All rights reserved.
+//
+
+import XCTest
+@testable import ParisHappnWeather
+
+class ParisHappnWeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
